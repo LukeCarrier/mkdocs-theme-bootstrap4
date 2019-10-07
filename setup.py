ae299774fb672b0b166bbf944c534c9002f7d9a7
Luke Carrier <luke@carrier.im>
2019-10-07 13:28:02 +0100

    Install bundle with package

--- setup.py
+++ setup.py
@@ -1,10 +1,48 @@
-from setuptools import setup
+from distutils.core import Command
+from distutils.errors import DistutilsOptionError
 from os import path
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.sdist import sdist
+import subprocess
 
 
-data_files = [
-    'css/font-awesome.min.css',
-]
+class BuildWebpackBundle(Command):
+    description = 'Install dependencies with Yarn and run Webpack'
+
+    user_options = [
+        ('webpack-environment=', None, 'Webpack environment ("development" or "production")')
+    ]
+
+    def initialize_options(self):
+        self.webpack_environment = None
+
+    def finalize_options(self):
+        self.set_undefined_options('sdist', ('webpack_environment', 'webpack_environment'))
+        if self.webpack_environment not in ('development', 'production'):
+            raise DistutilsOptionError('Must set the Webpack environment to either "development" or "production"; was {}'.format(
+                    self.webpack_environment))
+
+    def run(self):
+        subprocess.check_call(['yarn', 'install'], cwd=root_dir)
+        subprocess.check_call(['yarn', 'run', 'webpack', '--mode', self.webpack_environment], cwd=root_dir)
+
+
+def with_build_webpack_bundle(command_class, default_webpack_environment=None):
+    class WithBuildWebpackBundle(command_class):
+        def initialize_options(self):
+            super().initialize_options()
+            self.webpack_environment = default_webpack_environment
+
+        def make_distribution(self):
+            self.run_command('build_webpack_bundle')
+            super().make_distribution()
+
+    WithBuildWebpackBundle.user_options.append((
+            'webpack-environment=', None, 'Webpack environment ("development" or "production")'))
+
+    return WithBuildWebpackBundle
+
 
 root_dir = path.abspath(path.dirname(__file__))
 with open(path.join(root_dir, 'README.md')) as f:
@@ -15,7 +53,26 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     version='0.1.2',
+    cmdclass={
+        'build_webpack_bundle': BuildWebpackBundle,
+        'develop': with_build_webpack_bundle(develop, 'development'),
+        'sdist': with_build_webpack_bundle(sdist, 'production'),
+    },
     packages=['mkdocs_bootstrap4'],
+    package_data={
+        'mkdocs_bootstrap4': [
+            'dist/bundle.js',
+            '404.html',
+            'base.html',
+            'content.html',
+            'keyboard-modal.html',
+            'main.html',
+            'mkdocs_theme.yml',
+            'nav-sub.html',
+            'search-modal.html',
+            'toc.html',
+        ],
+    },
     url='https://github.com/byrnereese/mkdocs-bootstrap4/',
     license='MIT',
     author='Byrne Reese',
